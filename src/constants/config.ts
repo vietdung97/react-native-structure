b132a609e665acde5ac3bc150360ca1b6a886357
vietdung123 <ngovietdung0112@gmail.com>
2022-11-20 18:54:47 +0700

    Update

---
+++ src/constants/config.ts
@@ -0,0 +1,17 @@
+import { Config } from 'react-native-config';
+
+export const EnvLinkedIn = {
+  LI_CLIENT_ID: Config.LI_CLIENT_ID,
+  LI_REDIRECT_URI: Config.LI_REDIRECT_URI,
+  LI_CLIENT_SECRET: Config.LI_CLIENT_SECRET,
+};
+
+export const EnvFacebook = {
+  FB_APP_ID: Config.FB_APP_ID,
+  FB_APP_SERCET: Config.FB_APP_SERCET,
+};
+
+export const EnvTwitter = {
+  TWITTER_KEY: Config.TWITTER_KEY,
+  TWITTER_SECRET: Config.TWITTER_SECRET,
+};
--- src/networking/config.ts
+++ src/networking/config.ts
@@ -5,22 +5,6 @@ export const socketURL = baseURL;
 export const socketChatURL = baseURL + '/chat';
 export const apiURL = baseURL + '/api/v1';
 
-export const EnvLinkedIn = {
-  LI_CLIENT_ID: Config.LI_CLIENT_ID,
-  LI_REDIRECT_URI: Config.LI_REDIRECT_URI,
-  LI_CLIENT_SECRET: Config.LI_CLIENT_SECRET,
-};
-
-export const EnvFacebook = {
-  FB_APP_ID: Config.FB_APP_ID,
-  FB_APP_SERCET: Config.FB_APP_SERCET,
-};
-
-export const EnvTwitter = {
-  TWITTER_KEY: Config.TWITTER_KEY,
-  TWITTER_SECRET: Config.TWITTER_SECRET,
-};
-
 export const headers = {
   'Content-Type': 'application/json',
 };
